730726b4c164aeef6fbc8a498ad31d0880f3e5a6
Serebryakov-M <serebryakov.mihail.s@gmail.com>
2020-01-12 03:25:46 +0200

    Первое задание

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const http = require('http');
+const port = 3000;
+const fs = require('fs');
+http.createServer(function(req, res){
+    if(req.url === '/') {
+        res.write('hello world');
+        res.end();
+    } else if (req.url === '/about') {
+        
+        res.end();
+        console.log('URL страницы: ' + req.url);
+    
+    }else if (req.url === '/contact') {
+        
+        // res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        let contact = fs.createReadStream(__dirname + '/contact.html','utf8');
+        contact.pipe(res);
+    }else {
+        res.write('Page not found');
+        res.end();
+    }
+    
+}).listen(port, function(){
+    console.log('Server at http://localhost:3000');
+});
